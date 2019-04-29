81cc084ad9228a61b9ebb1a22978cfd7c645046b
bblinder <filmmaven@gmail.com>
2019-04-29 12:45:12 -0400

    Quick HTML change?

--- DevOps_script.sh
+++ DevOps_script.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-git_commit_push(){
+git_commit(){
     git add -A
-    git commit -m "Quick HTML Change"
-    git push -u origin
+    git commit -m "Quick HTML change"?
+    git push -u origin 0cdc3be8-7cf3-4b19-b4f3-3a4db8107c73
 }
 
 fast_purge(){
     akamai purge --edgerc ~/Downloads/edgerc --section purge delete https://devops-demo.akamai.com/0cdc3be8-7cf3-4b19-b4f3-3a4db8107c73/index.html
 }
 
-git_commit_push ; fast_purge
+git_commit ; fast_purge
